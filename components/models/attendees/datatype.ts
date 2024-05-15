dd7882adefd23e8eb8ae3ffd7e44f3b0c7b2c22f
Md Nasirul Haque <talk2lurisan@gmail.com>
2024-05-16 01:40:01 +0530

    used for redirecting to main site

--- components/models/attendees/datatype.ts
+++
@@ -1,8 +0,0 @@
-export interface Attendee {
-    id: number;
-    status: string;
-    created_at: string;
-    updated_at: string;
-    user: number;
-    event: number;
-}
--- next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  productionBrowserSourceMaps: false,
-  reactStrictMode: true,
-};
-
-export default nextConfig;
